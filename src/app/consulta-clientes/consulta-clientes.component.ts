cde32cb64c9bd74a35cc5c0ee6658477fd387ff5
Eletr0n1c <daniel.moraes@Tivit831.onmicrosoft.com>
2024-08-07 12:40:02 -0300

    feat: Cadastro de clientes com formulários reativos e exclusão de clientes

--- src/app/consulta-clientes/consulta-clientes.component.ts
+++ src/app/consulta-clientes/consulta-clientes.component.ts
@@ -6,27 +6,44 @@ import { environment } from '../../environments/environment';
 @Component({
   selector: 'app-consulta-clientes',
   standalone: true,
-  imports: [CommonModule,],
+  imports: [CommonModule],
   templateUrl: './consulta-clientes.component.html',
-  styleUrl: './consulta-clientes.component.css'
+  styleUrl: './consulta-clientes.component.css',
 })
 export class ConsultaClientesComponent {
-   //atributos
-   clientes: any[] = []; //array de objetos vazio
+  //atributos
+  clientes: any[] = []; //array de objetos vazio
+  mensagem: string = ' '; //texto vazio
 
-   //método construtor (inicilizando a biblioteca HttpClient)
-   constructor(private httpClient: HttpClient) {}
- 
-   //função executada ao abrir a página
-   ngOnInit() {
-     //fazendo uma chamada / requisição para consultar os clientes da API
-     this.httpClient.get(environment.apiClientes).subscribe({
-       //aguardando o retorno obtido da consulta
-       next: (data) => {
-         //capturando os dados da consulta
-         //armazenar os dados obtidos da API dentro do array clientes
-         this.clientes = data as any[];
-       },
-     });
-   }
+  //método construtor (inicilizando a biblioteca HttpClient)
+  constructor(private httpClient: HttpClient) {}
+
+  //função executada ao abrir a página
+  ngOnInit() {
+    //fazendo uma chamada / requisição para consultar os clientes da API
+    this.httpClient.get(environment.apiClientes).subscribe({
+      //aguardando o retorno obtido da consulta
+      next: (data) => {
+        //capturando os dados da consulta
+        //armazenar os dados obtidos da API dentro do array clientes
+        this.clientes = data as any[];
+      },
+    });
+  }
+
+  onDelete(id: string) {
+    //exibindo uma janela de confirmação
+    if (confirm('Deseja realmente excluir o cliente selecionado?')) {
+      //fazendo uma requisição DELETE para a API
+      this.httpClient.delete(environment.apiClientes + '/' + id).subscribe({
+        next: (data: any) => {
+          //exibindo a mensagem na página
+          this.mensagem = data.mensagem;
+
+          //fazer uma nova consulta na API
+          this.ngOnInit();
+        },
+      });
+    }
+  }
 }
